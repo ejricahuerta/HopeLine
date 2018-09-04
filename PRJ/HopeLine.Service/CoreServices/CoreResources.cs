12f709e69964484e9f619467f0e3a61dd3ff1496
ejricahuerta <ejricahuerta@SYCCE-19-3A89>
2018-09-04 19:53:49 -0400

    core resources add interface and implementation

---
+++ PRJ/HopeLine.Service/CoreServices/CoreResources.cs
@@ -0,0 +1,29 @@
+﻿using HopeLine.DataAccess.Entities;
+using HopeLine.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HopeLine.Service.CoreServices
+{
+    public class CoreResources : IResources
+    {
+        public Map DefaultMap()
+        {
+            //TODO : implement
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Community> GetCommunities()
+        {
+            //TODO : implement
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Resource> GetResources()
+        {
+            //TODO : implement
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PRJ/HopeLine.Service/Interfaces/IResources.cs
@@ -0,0 +1,15 @@
+﻿using HopeLine.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HopeLine.Service.Interfaces
+{
+    public interface IResources
+    {
+        IEnumerable<Community> GetCommunities();
+        IEnumerable<Resource> GetResources();
+
+        Map DefaultMap();
+    }
+}
