e74483d091bc9ac88509c93070260ddc68d35613
ejricahuerta <ejricahuerta@SYCCE-19-3A89>
2018-09-04 16:44:44 -0400

    INIT

---
+++ PRJ/HopeLine.API/Configurations/Mapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HopeLine.API.Configurations
+{
+
+    //TODO : Replace with mapper if needed or some other config
+    public class Mapper
+    {
+    }
+}
---
+++ PRJ/HopeLine.API/Controllers/MapController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HopeLine.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MapController : ControllerBase
+    {
+       //TODO : Filter
+
+       //TODO : Search
+
+    }
+}
