cedfb117d43ba1aeff0e33406ca26318884bb141
exricahuerta@gmail.com <exricahuerta@gmail.com>
2018-09-06 20:52:39 -0500

    add todo refresh token

--- PRJ/HopeLine.API/Controllers/AuthController.cs
+++ PRJ/HopeLine.API/Controllers/AuthController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace HopeLine.API.Controllers
 {
@@ -17,6 +12,6 @@ namespace HopeLine.API.Controllers
 
         //TODO : Register
 
-        //TODO : Refresh
+        //TODO : Refresh Token
     }
 }
