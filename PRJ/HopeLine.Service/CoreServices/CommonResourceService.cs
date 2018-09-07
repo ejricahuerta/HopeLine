3770150a8879e2bb8e91c61484db15f27a019ebd
ejricahuerta <ejricahuerta@SYCCA-12-4376>
2018-09-07 17:45:53 -0400

    constructor inject with repositories. consider unit of work pattern - to_be_discussed

--- PRJ/HopeLine.Service/CoreServices/CommonResourceService.cs
+++ PRJ/HopeLine.Service/CoreServices/CommonResourceService.cs
@@ -1,7 +1,20 @@
-﻿namespace HopeLine.Service.CoreServices
+﻿using HopeLine.DataAccess.Entities;
+using HopeLine.DataAccess.Interfaces;
+
+namespace HopeLine.Service.CoreServices
 {
     //TODO : implement interface
     public class CommonResourceService //: ICommonResource
     {
+        private readonly IRepository<Resource> _resourceRepo;
+        private readonly IRepository<Community> _communityRepo;
+
+        public CommonResourceService(IRepository<Resource> resourceRepo,
+                                    IRepository<Community> communityRepo)
+        {
+            _resourceRepo = resourceRepo;
+            _communityRepo = communityRepo;
+
+        }
     }
 }
--- PRJ/HopeLine.Service/CoreServices/CommunicationService.cs
+++ PRJ/HopeLine.Service/CoreServices/CommunicationService.cs
@@ -1,8 +1,20 @@
 ﻿//using HopeLine.Service.Interfaces;
 
+using HopeLine.DataAccess.Entities;
+using HopeLine.DataAccess.Interfaces;
+
 namespace HopeLine.Service.CoreServices
 {
     public class CommunicationService //: ICommunication
     {
+        private readonly IRepository<HopeLineUser> _userRepo;
+        private readonly IRepository<Conversation> _conversationRepo;
+
+        public CommunicationService(IRepository<HopeLineUser> userRepo,
+                                    IRepository<Conversation> conversationRepo)
+        {
+            _userRepo = userRepo;
+            _conversationRepo = conversationRepo;
+        }
     }
 }
