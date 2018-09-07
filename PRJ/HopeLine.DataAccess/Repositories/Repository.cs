ea8e413be1304144c4ccd3d6cca1990afbaaa86c
ejricahuerta <ejricahuerta@SYCCA-12-4376>
2018-09-07 16:33:24 -0400

    include generic repo and user repo with constructor

---
+++ PRJ/HopeLine.DataAccess/Repositories/Repository.cs
@@ -0,0 +1,60 @@
+﻿using HopeLine.DataAccess.DatabaseContexts;
+using HopeLine.DataAccess.Entities.Base;
+using HopeLine.DataAccess.Interfaces;
+using System.Collections.Generic;
+
+namespace HopeLine.DataAccess.Repositories
+{
+
+    //TODO : add functionalities and implementation
+    /// <summary>
+    /// Generic Repository
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly HopeLineDbContext _hopeLineDb;
+        private readonly ResourcesDbContext _resourcesDb;
+
+        public Repository(HopeLineDbContext hopeLineDb, ResourcesDbContext resourcesDb)
+        {
+            _hopeLineDb = hopeLineDb;
+            _resourcesDb = resourcesDb;
+        }
+
+        public void Delete(T obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Get(object id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Insert(T obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remove(object id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(T obj)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ PRJ/HopeLine.DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,85 @@
+﻿using HopeLine.DataAccess.DatabaseContexts;
+using HopeLine.DataAccess.Entities;
+using HopeLine.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace HopeLine.DataAccess.Repositories
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class UserRepository : IRepository<HopeLineUser>
+    {
+        private readonly HopeLineDbContext _hopeLineDb;
+
+
+        public UserRepository(HopeLineDbContext hopeLineDb)
+        {
+            _hopeLineDb = hopeLineDb;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        public void Delete(HopeLineUser obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public HopeLineUser Get(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<HopeLineUser> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        public void Insert(HopeLineUser obj)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        public void Remove(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        public void Update(HopeLineUser obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
