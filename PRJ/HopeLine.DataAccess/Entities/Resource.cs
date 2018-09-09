12c9ae93a8dd1044005d1aab96f7d60f4b08dab1
eosorio1 <eosorio1@SYCCE-04-6130>
2018-09-09 16:26:27 -0400

    add properties

--- PRJ/HopeLine.DataAccess/Entities/Resource.cs
+++ PRJ/HopeLine.DataAccess/Entities/Resource.cs
@@ -1,4 +1,5 @@
 ﻿using HopeLine.DataAccess.Entities.Base;
+using System.ComponentModel.DataAnnotations;
 
 namespace HopeLine.DataAccess.Entities
 {
@@ -10,5 +11,19 @@ namespace HopeLine.DataAccess.Entities
     public class Resource : CommonEntity
     {
 
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(100)]
+        public string Url { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(500)]
+        public string ImgUrl { get; set; }
     }
 }
