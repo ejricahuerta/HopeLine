473ce670604ad1818eefaf7907487b32c04e1f19
ejricahuerta <ejricahuerta@SYCCA-03-AF0A>
2018-09-10 18:18:41 -0400

    major patch for entities

--- PRJ/HopeLine.DataAccess/Entities/Base/CommonEntity.cs
+++ PRJ/HopeLine.DataAccess/Entities/Base/CommonEntity.cs
@@ -4,6 +4,10 @@ namespace HopeLine.DataAccess.Entities.Base
 {
     public class CommonEntity : IEntity
     {
+        public CommonEntity()
+        {
+            DateAdded = DateTime.UtcNow;
+        }
         public int Id { get; set; }
         public DateTime DateAdded { get; set; }
     }
