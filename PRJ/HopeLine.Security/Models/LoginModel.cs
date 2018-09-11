b08da3c2fc4c73751cec8a703ee5022a5fc07205
ejricahuerta <ejricahuerta@SYCCF-18-1FBA>
2018-09-11 18:09:29 -0400

    model with annotations

--- PRJ/HopeLine.Security/Models/LoginModel.cs
+++ PRJ/HopeLine.Security/Models/LoginModel.cs
@@ -1,8 +1,14 @@
-﻿namespace HopeLine.Security.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HopeLine.Security.Models
 {
     public class LoginModel
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
         public string Password { get; set; }
     }
 }
+
