6c9f8330d1657e5afa9dcd11d1d04c61fb2e42d9
ejricahuerta <ejricahuerta@SYCCF-18-1FBA>
2018-09-11 18:08:28 -0400

    jwt configuration update

---
+++ PRJ/HopeLine.Service/Configurations/ConfigureServiceExtension.cs
@@ -0,0 +1,76 @@
+﻿using HopeLine.DataAccess.DatabaseContexts;
+using HopeLine.DataAccess.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace HopeLine.Service.Configurations
+{
+    public class ConfigureServiceExtension
+    {
+
+        /// <summary>
+        /// Extended middleware config to separate classes.
+        /// </summary>
+        /// <param name="services"></param>
+        public static void AddConfiguration(IServiceCollection services)
+        {
+            services.AddDbContext<HopeLineDbContext>(opt => opt
+                                                .UseSqlServer("Server=tcp:prj.database.windows.net,1433;Initial Catalog=HopeLineDB;Persist Security Info=False;User ID=hopeline;Password=Prjgroup7;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
+            services.AddIdentity<HopeLineUser, IdentityRole>()
+                .AddEntityFrameworkStores<HopeLineDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Default Password settings.
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 4;
+                options.Password.RequiredUniqueChars = 1;
+            });
+
+            //JWT Authentication
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(
+                config =>
+                {
+                    config.RequireHttpsMetadata = false;
+                    config.SaveToken = true;
+                    config.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidIssuer = "http://localhost:5000",
+                        ValidAudience = "http://localhost:5000",
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SomeSecretofGroup7")),
+                        ClockSkew = TimeSpan.Zero
+                    };
+
+                });
+
+
+        }
+
+        /// <summary>
+        /// Add extenstion here 
+        /// </summary>
+        /// <param name="app"></param>
+        public static void Configure(IApplicationBuilder app)
+        {
+
+        }
+    }
+}
