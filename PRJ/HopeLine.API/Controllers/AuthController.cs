8aaf77ef3885a5c5c0d4cf2d6dee53fa2a0a8d64
ejricahuerta <ejricahuerta@SYCCF-18-1FBA>
2018-09-11 18:11:32 -0400

    new actions for simplier version of auth

--- PRJ/HopeLine.API/Controllers/AuthController.cs
+++ PRJ/HopeLine.API/Controllers/AuthController.cs
@@ -3,6 +3,13 @@ using HopeLine.Security.Interfaces;
 using HopeLine.Security.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace HopeLine.API.Controllers
@@ -16,39 +23,56 @@ namespace HopeLine.API.Controllers
     {
         private readonly UserManager<HopeLineUser> _userManager;
         private readonly SignInManager<HopeLineUser> _signInManager;
-        private readonly ITokenService _tokenService;
+
 
         //private readonly IUserService _userService;
 
         public AuthController(/*IUserService userService*/UserManager<HopeLineUser> userManager,
                                 SignInManager<HopeLineUser> signInManager, ITokenService tokenService)
         {
-            //_userService = userService;
             _userManager = userManager;
             _signInManager = signInManager;
-            _tokenService = tokenService;
+
         }
+
         [HttpPost]
-        [Route("login")]
-        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        [ValidateAntiForgeryToken]
+        public async Task<object> Login([FromBody] LoginModel model)
         {
-            var user = await _userManager.FindByEmailAsync(model.Username);
+            var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, false, false);
 
-            if (user != null)
+            if (result.Succeeded)
             {
-                var result = await _signInManager.PasswordSignInAsync(user, model.Password, false, false);
-
-                if (result.Succeeded)
-                {
-                    return Ok(_tokenService.GenerateJwtToken(model.Username, user));
-                }
-
+                var user = _userManager.Users.SingleOrDefault(u => u.Email == model.Username);
+                return GenerateToken(model.Username, user);
             }
-
-            return Unauthorized();
-
+            throw new ApplicationException("UNTRACED ERROR!");
         }
 
+        private object GenerateToken(string username, HopeLineUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, username),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Id)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SomeSecretofGroup7"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expires = DateTime.Now.AddDays(Convert.ToDouble(30));
+
+            var token = new JwtSecurityToken(
+                "https://localhost:5000",
+                "https://localhost:5000",
+                claims,
+                expires: expires,
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
 
 
         //TODO : Logout
