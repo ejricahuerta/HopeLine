034409ed4e1c951baaf93c5b7a85fdbe8abe1941
ejricahuerta <ejricahuerta@mysene.ca>
2018-09-11 03:57:51 -0400

    user dto with properties

--- PRJ/HopeLine.Security/Models/UserModel.cs
+++ PRJ/HopeLine.Security/Models/UserModel.cs
@@ -7,5 +7,9 @@
     /// </summary>
     public class UserModel
     {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
