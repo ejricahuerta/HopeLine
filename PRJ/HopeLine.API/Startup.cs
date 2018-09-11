ffa54af462c8f5931261bcd92c6135940798c47d
ejricahuerta <ejricahuerta@mysene.ca>
2018-09-11 03:57:10 -0400

    middleware configuration include jwt

--- PRJ/HopeLine.API/Startup.cs
+++ PRJ/HopeLine.API/Startup.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using HopeLine.DataAccess.DatabaseContexts;
+using HopeLine.DataAccess.Entities;
+using HopeLine.Security.Interfaces;
+using HopeLine.Security.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace HopeLine.API
 {
@@ -26,14 +28,38 @@ namespace HopeLine.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<HopeLineDbContext>(opt =>
+                opt.UseSqlServer("Server=tcp:prj.database.windows.net,1433;Initial Catalog=HopeLineDB;Persist Security Info=False;User ID=hopeline;Password=Prjgroup7;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
+            services.AddIdentity<HopeLineUser, IdentityRole>(opt => { })
+                .AddEntityFrameworkStores<HopeLineDbContext>()
+                .AddDefaultTokenProviders();
+
+            //TODO : Add JWT Config
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opt =>
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidIssuer = "http://localhost:44340", //TODO : move to some class
+                        ValidAudience = "http://localhost:44340", //TODO : move to some class
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("somesecretkey")) // TODO : use appsettings
+                    }
+                );
+
+
+
+
 
             //TODO : Add Dependency Injections
-
+            services.AddTransient<ITokenService, TokenService>();
             //services.AddTransient<interface,implementationClass>();
 
             //TODO : Add SignalR
 
-            //TODO : Add JWT Config
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,7 +73,7 @@ namespace HopeLine.API
             {
                 app.UseHsts();
             }
-
+            app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
