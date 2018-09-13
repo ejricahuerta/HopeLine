dd9d5d86129b24ea9c201a50bc965296e10726a1
ejricahuerta <ejricahuerta@mysene.ca>
2018-09-12 23:54:17 -0400

    new user model data

---
+++ PRJ/HopeLine.Service/Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using static HopeLine.DataAccess.Entities.HopeLineUser;
+
+namespace HopeLine.Service.Models
+{
+
+    /// <summary>
+    /// DTO fo users. this does not contain any logic at all.
+    /// it is a Dummy data transfer object
+    /// </summary>
+    public class UserModel
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Phone { get; set; }
+        public ICollection<string> Languages { get; set; }
+        public Account AccountType { get; set; }
+    }
+
+}
