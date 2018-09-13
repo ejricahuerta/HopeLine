b3ed8816ffb2d328a79d75b7723c55f2759ad3cb
ejricahuerta <ejricahuerta@mysene.ca>
2018-09-13 05:34:17 -0400

    add more functionalities to implement

--- PRJ/HopeLine.Service/Interfaces/IUserService.cs
+++ PRJ/HopeLine.Service/Interfaces/IUserService.cs
@@ -10,11 +10,16 @@ namespace HopeLine.Service.Interfaces
         IEnumerable<UserModel> GetAllUsers();
         IEnumerable<UserModel> GetAllUsersByAccountType(Account accountType);
 
-        #region can be merged
+
+        //Can be refactored
+        #region Users and Mentors
+
         IEnumerable<ActivityModel> GetUserActivities(string userId);
         IEnumerable<ActivityModel> GetMentorActivities(string mentorId);
-        IEnumerable<ConversationModel> GetUserConversations(string userId);
+        IEnumerable<ConversationModel> GetUserConversations(string username);
         IEnumerable<ConversationModel> GetMentorConversations(string mentorId);
+        IEnumerable<ScheduleModel> GetMentorSchedules(string mentorId);
+        IEnumerable<SpecializationModel> GetMentorSpecializations(string mentorId);
         #endregion
 
         bool UpdateUserProfile(UserModel model);
