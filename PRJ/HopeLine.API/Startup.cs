fe03d9e86e3731d9af3b64dbf776776af944350f
ejricahuerta <ejricahuerta@mysene.ca>
2018-09-13 05:54:08 -0400

    signalr core include

--- PRJ/HopeLine.API/Startup.cs
+++ PRJ/HopeLine.API/Startup.cs
@@ -1,12 +1,16 @@
-﻿using HopeLine.Security.Interfaces;
+﻿using HopeLine.API.Hubs;
+using HopeLine.Security.Interfaces;
 using HopeLine.Security.Services;
 using HopeLine.Service.Configurations;
+using HopeLine.Service.CoreServices;
+using HopeLine.Service.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Http;
+
 namespace HopeLine.API
 {
     public class Startup
@@ -21,23 +25,21 @@ namespace HopeLine.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             ConfigureServiceExtension.AddConfiguration(services);
 
 
             services.AddCors();
             services.AddLogging();
-            services.AddMvc();
-
+            services.AddSignalR();
 
             services.AddSingleton<ITokenService, TokenService>();
-
+            services.AddTransient<IUserService, UserService>();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app,
-                                IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -57,12 +59,18 @@ namespace HopeLine.API
                 });
 
             app.UseCors(opt =>
-            opt.AllowAnyHeader()
-                .AllowCredentials()
-                .AllowAnyOrigin());
+                opt.AllowAnyHeader()
+                    .AllowCredentials()
+                    .AllowAnyOrigin());
 
             app.UseAuthentication();
             app.UseMvc();
+
+            app.UseSignalR(route =>
+            {
+                route.MapHub<ChatHub>("/chat");
+            });
+
 
             ConfigureServiceExtension.UseConfiguration(app);
 
