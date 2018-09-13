4787762f4914629ce3a7d6eb0906e4514eca2326
ejricahuerta <ejricahuerta@mysene.ca>
2018-09-13 05:53:48 -0400

    inject userservice to controller

--- PRJ/HopeLine.API/Controllers/UserController.cs
+++ PRJ/HopeLine.API/Controllers/UserController.cs
@@ -1,13 +1,17 @@
+using HopeLine.Service.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 
 [Authorize]
 [ApiController]
-public class UserController : ControllerBase {
-public UserController(IUserService)
+public class UserController : ControllerBase
 {
-    
-}
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
 
 }
