213431150666de2fae136a3cb172be7ce3dc61a5
ejricahuerta <ejricahuerta@mysene.ca>
2018-09-13 05:54:38 -0400

    no implementaton, but initialize

--- PRJ/HopeLine.Service/CoreServices/UserService.cs
+++ PRJ/HopeLine.Service/CoreServices/UserService.cs
@@ -1,10 +1,60 @@
-﻿using System;
+﻿using HopeLine.DataAccess.Entities;
+using HopeLine.Service.Interfaces;
+using HopeLine.Service.Models;
 using System.Collections.Generic;
-using System.Text;
 
 namespace HopeLine.Service.CoreServices
 {
-    class UserService
+    public class UserService : IUserService
     {
+        public IEnumerable<UserModel> GetAllUsers()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<UserModel> GetAllUsersByAccountType(HopeLineUser.Account accountType)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<ActivityModel> GetMentorActivities(string mentorId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<ConversationModel> GetMentorConversations(string mentorId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<ScheduleModel> GetMentorSchedules(string mentorId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<SpecializationModel> GetMentorSpecializations(string mentorId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<ActivityModel> GetUserActivities(string userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<ConversationModel> GetUserConversations(string username)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool UpdateUserLogin(UserModel model, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool UpdateUserProfile(UserModel model)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
