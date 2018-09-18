9161c626572a894c31df371a79b021b8619ffc44
Jasper-Shi <31856111+Jasper-Shi@users.noreply.github.com>
2018-09-18 14:29:04 -0400

    razor pages

--- PRJ/HopeLine.Web/wwwroot/js/site.js
+++ PRJ/HopeLine.Web/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+//---------------------------------------//
+//         Footer                       //
+//---------------------------------------//
+
+var d = new Date().getFullYear() + " &copy;  - HopeLine   ";
+
+document.getElementById("demo").innerHTML = d;
+
+
+
+
+
+
+
+
