55a914498da0d5c36a6ce8abdacb3191c4b4c01a
ejricahuerta <ejricahuerta@SYCCC-15-82A0>
2018-09-19 17:07:09 -0400

    initial for chat hub

--- PRJ/HopeLine.API/Hubs/ChatHub.cs
+++ PRJ/HopeLine.API/Hubs/ChatHub.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using HopeLine.Service.CoreServices;
+using HopeLine.Service.Interfaces;
+using HopeLine.Service.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Threading.Tasks;
@@ -12,11 +15,26 @@ namespace HopeLine.API.Hubs
     /// </summary>
     /// 
     [Authorize]
+
     public class ChatHub : Hub
     {
+        private readonly ICommunication _communicationService;
 
+        public ChatHub(ICommunication communicationService)
+        {
+            _communicationService = communicationService;
+        }
         public override Task OnConnectedAsync()
         {
+
+            Console.WriteLine("Mentor Connected");
+            _communicationService.AddConversation(new ConversationModel
+            {
+                DateOfConversation = DateTime.UtcNow,
+                // connection ID
+                //
+            });
+
             return base.OnConnectedAsync();
         }
 
@@ -29,5 +47,20 @@ namespace HopeLine.API.Hubs
         {
             base.Dispose(disposing);
         }
+
+        public Task SendToMentor(string mentor, string message)
+        {
+            return Clients.User(mentor).SendAsync(message);
+        }
+
+        public async Task AddToGroup(string groupName)
+        {
+            Console.WriteLine("Created new group: Room ID");
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Group(groupName).SendAsync("Send", "User Joined");
+
+        }
+
+
     }
 }
