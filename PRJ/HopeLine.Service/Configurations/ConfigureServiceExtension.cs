671e78e25fef6cc66cf1e19244328b5b4e98ac11
ejricahuerta <exricahuerta@gmail.com>
2018-09-22 17:20:49 -0400

    db and service for messaging

--- PRJ/HopeLine.Service/Configurations/ConfigureServiceExtension.cs
+++ PRJ/HopeLine.Service/Configurations/ConfigureServiceExtension.cs
@@ -64,6 +64,9 @@ namespace HopeLine.Service.Configurations
 
                 });
 
+            services.AddDbContext<ChatDbContext>(opt =>
+                            opt.UseInMemoryDatabase("chatdb"));
+
             services.AddDbContext<ResourcesDbContext>(opt => opt
                                                           .UseSqlServer(APIConstant.ConnectionString));
             services.AddDbContext<HopeLineDbContext>(opt => opt
---
+++ PRJ/HopeLine.Service/Models/MessageModel.cs
@@ -0,0 +1,9 @@
+namespace HopeLine.Service.Models
+{
+    public class MessageModel
+    {
+        public string ConnectionId { get; set; }
+        public string UserName { get; set; }
+        public string Text { get; set; }
+    }
+}
