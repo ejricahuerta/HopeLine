ff8303c6127fa7f123895e0f926e8ee38a458270
ejricahuerta <exricahuerta@gmail.com>
2018-09-22 18:32:40 -0400

    load message implemented

--- PRJ/HopeLine.API/Hubs/ChatHub.cs
+++ PRJ/HopeLine.API/Hubs/ChatHub.cs
@@ -1,6 +1,7 @@
 ﻿using HopeLine.Service.Interfaces;
 using HopeLine.Service.Models;
 using Microsoft.AspNetCore.SignalR;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace HopeLine.API.Hubs
@@ -22,7 +23,7 @@ namespace HopeLine.API.Hubs
         public async Task AddUserToRoom(string room)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, room);
-            System.Console.WriteLine("Added User");
+            System.Console.WriteLine("Added User: " + Context.ConnectionId);
         }
 
         public async Task RemoveUserFromRoom(string room)
@@ -38,6 +39,27 @@ namespace HopeLine.API.Hubs
             }
         }
 
+        public async Task LoadMessage(string room)
+        {
+            try
+            {
+                System.Console.WriteLine(room);
+                var allMessages = _messageService.GetAllMessages(room);
+                System.Console.WriteLine(" Count: " +allMessages.Count());
+                if (allMessages != null)
+                {
+                    foreach (var m in allMessages)
+                    {
+                         await Clients.Caller.SendAsync("Load", m.UserName, m.Text);
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+
+                throw new System.Exception("Unable to Load Data: ", ex);
+            }
+        }
 
         public async Task SendMessage(string user, string message, string room)
         {
