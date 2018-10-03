35878c1f3558d90476c7c2bd2d0efddab8e8b9b1
ejricahuerta <exricahuerta@gmail.com>
2018-10-03 11:40:20 -0400

    mentor login and redirect  (#50)

--- PRJ/HopeLine.Security/Services/TokenService.cs
+++ PRJ/HopeLine.Security/Services/TokenService.cs
@@ -117,20 +117,29 @@ namespace HopeLine.Security.Services
 
         public async Task<object> RegisterUser(RegisterModel model)
         {
-            var user = new MentorAccount
+            try
             {
-                Profile = new Profile
+                var user = new MentorAccount
                 {
-                    FirstName = model.FirstName,
-                    LastName = model.LastName
-                },
-                UserName = model.Username,
-                Email = model.Username
-            };
-            var result = await _userManager.CreateAsync(user, model.Password);
-            if (result.Succeeded)
+                    Profile = new Profile
+                    {
+                        FirstName = model.FirstName,
+                        LastName = model.LastName
+                    },
+                    UserName = model.Username,
+                    Email = model.Username
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return GenerateToken(model.Username, user);
+
+                }
+            }
+            catch (System.Exception ex)
             {
-                return GenerateToken(model.Username, user);
+
+                throw new Exception("Unable to Create User: ", ex);
             }
             return null;
         }
