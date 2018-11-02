37965f99c92c4ae1bf99fea4ff1c126f0e7c8db0
IDEddy <edgar_giang@hotmail.com>
2018-11-02 10:55:42 -0400

    Update to the Logic for proper map filtering before pushing to master(#77) -Html page will be updated as well soon

--- PRJ/HopeLine.Web/wwwroot/js/map.js
+++ PRJ/HopeLine.Web/wwwroot/js/map.js
@@ -1,19 +1,23 @@
-﻿// This example adds a search box to a map, using the Google Place Autocomplete
-// feature. People can enter geographical searches. The search box will return a
-// pick list containing a mix of places and predicted search terms.
+﻿var searchBox, input, bounds, locationType, marker, placeLoc, places;
+var results = [];
+var markers = [];
+var tempResults = [];
 
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-//initialized variables.
-var searchBox, input, bounds, locationType, marker, placeLoc, places;
-var pos;
+var pos = {
+    lat: position.coords.latitude,
+    lng: position.coords.longitude
+}
 
+
+/* When the page is loaded initAutocompelte would be called to generate the map*/
 function initAutocomplete() {
+    /*Default generated position is Seneca College@York */
     map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 43.78, lng: -79.49 },
+
+        center: { lat: 43.77, lng: -79.49 },
         zoom: 13,
         mapTypeId: 'roadmap'
+
     });
 
     //load the places(Google api feature) service map
@@ -22,18 +26,21 @@ function initAutocomplete() {
     input = document.getElementById('pac-input');
     searchBox = new google.maps.places.SearchBox(input);
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-    //////////////////////////////////////////////
+    ////////////////////////////////////////////// 
     bounds = new google.maps.LatLngBounds();
 
     if (navigator.geolocation) {
+
         navigator.geolocation.getCurrentPosition(function (position) {
+
             pos = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
+
             };
 
             infoWindow.setPosition(pos);
-            infoWindow.setContent('You are here');
+            infoWindow.setContent('You are here!');
             infoWindow.open(map);
             map.setCenter(pos);
 
@@ -49,44 +56,50 @@ function initAutocomplete() {
     map.addListener('bounds_changed', function () {
         searchBox.setBounds(map.getBounds());
     });
-
-    var markers = [];
+    markers = [];
+    //  var markers = [];
     // Listen for the event fired when the user selects a prediction and retrieve
     // more details for that place.
     searchBox.addListener('places_changed', function () {
+
         places = searchBox.getPlaces();
 
         if (places.length == 0) {
             return;
         }
-
         // Clear out the old markers.
         markers.forEach(function (marker) {
             marker.setMap(null);
+
         });
+
         markers = [];
-
         // For each place, get the icon, name and location.
-
         places.forEach(function (place) {
+
             if (!place.geometry) {
                 console.log("Returned place contains no geometry");
                 return;
             }
+
             var icon = {
+
                 url: place.icon,
                 size: new google.maps.Size(71, 71),
                 origin: new google.maps.Point(0, 0),
                 anchor: new google.maps.Point(17, 34),
                 scaledSize: new google.maps.Size(25, 25)
+
             };
 
             // Create a marker for each place.
             markers.push(new google.maps.Marker({
+
                 map: map,
                 icon: icon,
                 title: place.name,
                 position: place.geometry.location
+
             }));
 
             if (place.geometry.viewport) {
@@ -95,22 +108,48 @@ function initAutocomplete() {
             } else {
                 bounds.extend(place.geometry.location);
             }
+
         });
         map.fitBounds(bounds);
-    });
 
+    });
 }
 
-function searchType(locationType) {
+/* Everytime a checkbox is checked or unchecked search type would
+be called and locationType data would be passed on. If
+It's checked call searchLocation(LocationType) else delete the markers*/
+function searchType(locationType, isChecked) {
+
+    if (isChecked.checked) {
+
+        searchLocation(locationType);
+    } else {
+        deleteMarkers(isChecked, locationType);
+
+    }
+
+}
+/*Generate infoWindow and service for the generated google map.
+Request variable would find the nearest locations (locatioNType) within it's
+radius. Callback function would be called passing in the results,
+status if callback is succesful and the location Type*/
+
+
+function searchLocation(locationType) {
+
     infowindow = new google.maps.InfoWindow();
     service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
+    var request = {
         location: pos,
-        radius: 5000,
+        radius: 9000,
         type: [locationType]
-    }, callback);
-
+    };
+    service.nearbySearch(request, function (results, status) {
+        callback(results, status, locationType);
+    });
 }
+
+/*If geolocation fails send a message  */
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
@@ -119,31 +158,40 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         'Error: Your browser doesn\'t support geolocation.');
     infoWindow.open(map);
 }
+//////////////////////////////////////////////////////
+/*Sets both filtered markers and google search markers to null */
+function setMapOnAll(map) {
+    for (var i = 0; i < results.length; i++) {
+        results[i].setMap(map);
 
-function callback(results, status) {
+    }
 
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-        }
-
+    for (var j = 0; j < markers.length; j++) {
+        markers[j].setMap(map);
     }
 }
 
-
-
-function createMarker(place) {
-    console.log("callback");
-    placeLoc = place.geometry.location;
-    marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-
-    });
-
-    google.maps.event.addListener(marker, 'click', function () {
-        infowindow.setContent(place.name);
-        infowindow.open(map, this);
-    });
+function clearMarkers() {
+    var checkboxes = document.getElementsByName('locationBox');
+    for (var i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = false;
+    }
+    setMapOnAll(null);
 }
+/*Scans through temporary array(tempResults) and checks if it's locationType
+property is equal to locationType of passed parameter.
+If true will set the markers to null. */
 
+function deleteMarkers(isChecked, locationType) {
+
+    for (var i = 0; i < tempResults.length; i++) {
+        // console.log(tempResults[i].locationType + "<-results[i].locationType INdforloop");
+        if (tempResults[i].locationType == locationType) {
+            // console.log(tempResults[i].locationType + "<-results[i].locationType IN DELETEMARKERS");
+            results[i].setMap(null);
+
+        }
+
+    }
+
+}
