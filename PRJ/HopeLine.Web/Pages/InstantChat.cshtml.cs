edb42dc835b579cd4e7bc4ffe760bd035dcb81d2
Jasper-Shi <31856111+Jasper-Shi@users.noreply.github.com>
2018-11-03 15:10:33 -0400

    #82 Chat-page template resolved

---
+++ PRJ/HopeLine.Web/Pages/InstantChat.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HopeLine.Web.Pages
+{
+    public class InstantChatModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
