976a6218741d1c38789ebc84cf740a73379c7cf0
Jasper-Shi <31856111+Jasper-Shi@users.noreply.github.com>
2018-11-04 12:53:43 -0500

    lay out change (#82)

--- PRJ/HopeLine.Web/Pages/InstantChat.cshtml.cs
+++ PRJ/HopeLine.Web/Pages/InstantChat.cshtml.cs
@@ -2,15 +2,60 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HopeLine.Service.CoreServices;
+using HopeLine.Service.Interfaces;
+using HopeLine.Service.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
 
 namespace HopeLine.Web.Pages
 {
     public class InstantChatModel : PageModel
     {
-        public void OnGet()
+        private readonly ICommonResource _commonResource;
+        private readonly ICommunication _communication;
+
+        public InstantChatModel(ICommunication communication, ICommonResource commonResource)
         {
+            _commonResource = commonResource;
+            _communication = communication;
+        }
+
+        [BindProperty]
+        public string PIN { get; set; }
+
+        [BindProperty]
+        public List<string> Topics { get; set; }
+
+        [BindProperty]
+        public string UserName { get; set; }
+        public string ReturnUrl { get; set; }
+
+        public IActionResult OnGet(string pin = null, string user = null)
+        {
+            UserName = "guest";
+            
+            if (UserName != null)
+            {
+                if (pin == null)
+                    PIN = _communication.GenerateConnectionId();
+                else
+                    PIN = pin;
+
+                ReturnUrl = Url.Content("~/" + "?pin=" + pin);
+                Topics = _commonResource.GetTopics().Select(t => t.Name).ToList();
+                return Page();
+
+            }
+            else
+            {
+                string url = Url.Page("/Login", new { area = "Guest", returnUrl = "chat" });
+                return LocalRedirect(url);
+            }
+        }
+        
         }
     }
 }
