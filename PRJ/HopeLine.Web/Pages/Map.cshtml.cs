3187aaf940c712b9d32301f356db1b36ffd912b1
Jasper-Shi <31856111+Jasper-Shi@users.noreply.github.com>
2018-11-18 16:46:34 -0500

    navbar fixed #98

--- PRJ/HopeLine.Web/Pages/Map.cshtml.cs
+++ PRJ/HopeLine.Web/Pages/Map.cshtml.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HopeLine.DataAccess.Entities;
+using HopeLine.Service.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +12,41 @@ namespace HopeLine.Web.Pages
 {
     public class MapModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ICommonResource _commonResource;
+        public readonly IUserService _userService;
+        public readonly UserManager<HopeLineUser> _userManager;
 
+        public MapModel(ICommonResource commonResources, IUserService userService, UserManager<HopeLineUser> userManager)
+        {
+            _commonResource = commonResources;
+            _userService = userService;
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public bool isUser { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            HopeLineUser CurrentUser = await _userManager.GetUserAsync(User);
+
+            if (CurrentUser != null)
+            {
+                if (CurrentUser.AccountType == DataAccess.Entities.Account.User || CurrentUser.AccountType == DataAccess.Entities.Account.Guest)
+                {
+                    isUser = true;
+                }
+                else
+                {
+                    isUser = false;
+                }
+            }
+            else
+            {
+                isUser = true;
+            }
+
+            return Page();
         }
     }
 }
