7c1b3b71c40a15235322c08f0d74120bb35a7aeb
Jasper-Shi <31856111+Jasper-Shi@users.noreply.github.com>
2018-11-19 10:07:04 -0500

    mentor navbar bug fixed #98

--- PRJ/HopeLine.Web/Pages/Index.cshtml.cs
+++ PRJ/HopeLine.Web/Pages/Index.cshtml.cs
@@ -1,19 +1,27 @@
-﻿using HopeLine.Service.Interfaces;
+﻿using HopeLine.DataAccess.Entities;
+using HopeLine.Service.Interfaces;
 using HopeLine.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace HopeLine.Web.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ICommonResource _commonResource;
-        public IndexModel(ICommonResource commonResource)
+        public readonly IUserService _userService;
+        public readonly UserManager<HopeLineUser> _userManager;
+
+        public IndexModel(ICommonResource commonResource, IUserService userService, UserManager<HopeLineUser> userManager)
         {
             _commonResource = commonResource;
+            _userService = userService;
+            _userManager = userManager;
         }
 
         public IList<string> Languages { get; set; }
@@ -22,9 +30,29 @@ namespace HopeLine.Web.Pages
         public IList<TopicViewModel> Topics { get; set; }
 
         [BindProperty]
+        public bool isUser { get; set; }
+
+        [BindProperty]
         public IList<TopicViewModel> TopicsSelected { get; set; }
-        public IActionResult OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
+            HopeLineUser CurrentUser = await _userManager.GetUserAsync(User);
+            if (CurrentUser != null)
+            {
+                if (CurrentUser.AccountType == DataAccess.Entities.Account.User || CurrentUser.AccountType == DataAccess.Entities.Account.Guest)
+                {
+                    isUser = true;
+                }
+                else
+                {
+                    isUser = false;
+                }
+            }
+            else
+            {
+                isUser = true;
+            }
+
             /*
             var claim = User.Claims.FirstOrDefault(u => u.Type == "Account");
             var url = Url.Page("/Index", new { Page = "Mentor" });
