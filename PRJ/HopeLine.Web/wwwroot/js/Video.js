965bdc5c6d788dd32d95591b2326e37590daf93a
Edmel John Ricahuerta <ejricahuerta@myseneca.ca>
2018-11-22 15:26:18 -0500

    clean up for scripts

--- PRJ/HopeLine.Web/wwwroot/js/Video.js
+++ PRJ/HopeLine.Web/wwwroot/js/Video.js
@@ -1,11 +1,12 @@
-
 //Twilio Connection API
 const video = Twilio.Video;
 
 let token = $("#token").val();
 
 console.log("token is: " + token);
-video.connect(token, { name: 'room-name' }).then(room => {
+video.connect(token, {
+    name: 'room-name'
+}).then(room => {
     console.log('Connected to Room ' + room.name + " token is: " + token);
 
     room.participants.forEach(participantConnected);
