a380a03c6d0640b20f3be8e9960c182ab5def671
Jasper-Shi <31856111+Jasper-Shi@users.noreply.github.com>
2018-11-24 20:19:36 -0500

    error page 404 resolved #99

--- PRJ/HopeLine.Web/Pages/Error.cshtml.cs
+++ PRJ/HopeLine.Web/Pages/Error.cshtml.cs
@@ -5,19 +5,39 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using HopeLine.Web;
 
 namespace HopeLine.Web.Pages
 {
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None,
+            NoStore = true)]
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
         }
     }
+
+    /* 
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public class ErrorModel : PageModel
+    {
+        [BindProperty]
+        public int code { get; set; }
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public void OnGet()
+        {
+            code = HopeLine.Web.Startup.getErrcode();
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        }
+    }
+    */
 }
