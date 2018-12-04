d34941384d091bb46189e5682f590fa178929b3f
ejricahuerta <ejricahuerta@mysene.ca>
2018-12-04 12:45:18 -0500

    add topics to room

--- PRJ/HopeLine.DataAccess/Entities/Room.cs
+++ PRJ/HopeLine.DataAccess/Entities/Room.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace HopeLine.DataAccess.Entities {
@@ -7,6 +9,7 @@ namespace HopeLine.DataAccess.Entities {
 
         public Room () {
             DateAdded = DateTime.Now.ToString ("MM/dd/yyyy HH:mm:ss");
+            Topics = new List<int> ();
         }
 
         [Key]
@@ -15,5 +18,6 @@ namespace HopeLine.DataAccess.Entities {
         public string RoomId { get; set; }
         public string GuestId { get; set; }
         public string DateAdded { get; set; }
+        public IList<int> Topics { get; set; }
     }
 }
