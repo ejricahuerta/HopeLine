91308cff3db420a113b08a99de66cabd57f16188
ejricahuerta <ejricahuerta@mysene.ca>
2018-12-04 12:45:01 -0500

    remove minutes from service

--- PRJ/HopeLine.Service/Models/ConversationModel.cs
+++ PRJ/HopeLine.Service/Models/ConversationModel.cs
@@ -13,7 +13,6 @@ namespace HopeLine.Service.Models {
         public string PIN { get; set; }
         public string MentorId { get; set; }
         public string UserId { get; set; }
-        public float Minutes { get; set; }
         public DateTime DateOfConversation { get; set; }
         public Rating Rating { get; set; }
     }
