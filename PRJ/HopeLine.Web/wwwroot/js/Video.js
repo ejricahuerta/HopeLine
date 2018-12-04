8610e89a92fca6d1230a7319dd60bda0462ee573
ejricahuerta <ejricahuerta@mysene.ca>
2018-12-04 12:44:07 -0500

    css style change

--- PRJ/HopeLine.Web/wwwroot/js/Video.js
+++ PRJ/HopeLine.Web/wwwroot/js/Video.js
@@ -15,130 +15,139 @@ var localVideoMedia;
 var localAudioMedia;
 
 var option = {
-    name: $("#roomId").val(), // Getting the room name to connect
-    audio: false, //Stop audio and video from connecting to the room automatically
-    video: false
-}
+  name: $("#roomId").val(), // Getting the room name to connect
+  audio: false, //Stop audio and video from connecting to the room automatically
+  video: false
+};
 
-Video.connect($("#token").val(), option).then(room => { // Connect the to room
+Video.connect(
+  $("#token").val(),
+  option
+).then(room => {
+  // Connect the to room
 
-    console.log('Connected to Room "%s"', room.name);
-    console.log('User name "%s"', userId);
+  console.log('Connected to Room "%s"', room.name);
+  console.log('User name "%s"', userId);
 
+  Video.createLocalVideoTrack().then(function(videoTrack) {
+    //Creating Video media
+    localVideoMedia = videoTrack; //Saving the video media to use later
+    $("#local-media").append(videoTrack.attach()); //Adding video media to our webpage
+    localVideoMedia.disable(); //Disable it
+  });
+  Video.createLocalAudioTrack().then(function(audioTrack) {
+    //Creating audio media
+    localAudioMedia = audioTrack; //Saving the audio media to use later
+    $("#local-media").append(audioTrack.attach()); //Adding audio media to our webpage
+    localAudioMedia.disable(); //Disable it
+  });
 
+  $("#close-button").click(function() {
+    //When the click on the close icon, disconnect from room and close window
+    room.disconnect();
 
-    Video.createLocalVideoTrack().then(function (videoTrack) { //Creating Video media
-        localVideoMedia = videoTrack; //Saving the video media to use later
-        $("#local-media").append(videoTrack.attach()); //Adding video media to our webpage
-        localVideoMedia.disable(); //Disable it
-    });
-    Video.createLocalAudioTrack().then(function (audioTrack) { //Creating audio media
-        localAudioMedia = audioTrack; //Saving the audio media to use later
-        $("#local-media").append(audioTrack.attach()); //Adding audio media to our webpage
-        localAudioMedia.disable(); //Disable it
-    })
+    window.close();
+  });
 
-    $("#close-button").click(function () { //When the click on the close icon, disconnect from room and close window
-        room.disconnect();
+  $("#mute-button").click(function() {
+    //When clicking the mute button
+    if (localAudioShow) {
+      //If not muted
+      localAudioMedia.disable(); //Disable self audio from own screen
+      localAudioShow = false;
+      room.localParticipant.unpublishTrack(localAudioMedia); //Disable self audio from the room
+    } else {
+      //If muted
+      localAudioMedia.enable(); //Enable self audio
+      localAudioShow = true;
+      room.localParticipant.publishTrack(localAudioMedia); //Enable self audio to room
+    }
+  });
 
-        window.close();
-    });
+  $("#video-button").click(function() {
+    //When clicking the video button
+    if (localVideoShow) {
+      //If video enabled
+      localVideoMedia.disable(); //Disable self video from own screen
+      localVideoShow = false;
+      room.localParticipant.unpublishTrack(localVideoMedia); //Disable self video from room
+    } else {
+      //If video showing
+      localVideoShow = true;
+      localVideoMedia.enable(); //Enable self video to own screen
+      room.localParticipant.publishTrack(localVideoMedia); //Enable self video to room
+    }
+  });
 
-    $("#mute-button").click(function () { //When clicking the mute button
-        if (localAudioShow) { //If not muted
-            localAudioMedia.disable(); //Disable self audio from own screen
-            localAudioShow = false;
-            room.localParticipant.unpublishTrack(localAudioMedia); //Disable self audio from the room
-        } else { //If muted
-            localAudioMedia.enable(); //Enable self audio
-            localAudioShow = true;
-            room.localParticipant.publishTrack(localAudioMedia); //Enable self audio to room
-        }
-    });
+  room.on("trackPublished", function() {
+    console.log("track was published");
+  });
+  room.on("trackUnpublished", function() {
+    console.log("track was unpublished");
+  });
 
+  room.on("trackDisabled", function() {
+    console.log("track was disabled");
+  });
 
-    $("#video-button").click(function () { //When clicking the video button
-        if (localVideoShow) { //If video enabled
-            localVideoMedia.disable(); //Disable self video from own screen
-            localVideoShow = false;
-            $("#local-media > video ").remove();
+  room.on("trackEnabled", function() {
+    console.log("track was enabled");
+  });
 
-            room.localParticipant.unpublishTrack(localVideoMedia); //Disable self video from room
-        } else { //If video showing
-            localVideoMedia.enable(); //Enable self video to own screen
-            room.localParticipant.publishTrack(localVideoMedia); //Enable self video to room
-            localVideoShow = true;
-            $("#local-media").append(localVideoMedia.attach());
-        }
-    });
+  room.participants.forEach(function(participant) {
+    // When joining the room, checks whos there
+    console.log("Already in Room: '" + participant.identity + "'");
+  });
 
-    room.on('trackPublished', function () {
-        console.log("track was published");
-    });
-    room.on('trackUnpublished', function () {
-        console.log("track was unpublished");
-    });
+  room.participants.forEach(participantConnected);
+  room.on("participantConnected", participantConnected); //When someone connects to room, run 'participantConnected function
 
-    room.on('trackDisabled', function () {
-        console.log("track was disabled");
-    });
-
-    room.on('trackEnabled', function () {
-        console.log("track was enabled");
-    });
-
-
-    room.participants.forEach(function (participant) { // When joining the room, checks whos there
-        console.log("Already in Room: '" + participant.identity + "'");
-    });
-
-
-
-    room.participants.forEach(participantConnected);
-    room.on('participantConnected', participantConnected); //When someone connects to room, run 'participantConnected function
-
-    room.on('participantDisconnected', participantDisconnected); //When someone disconnects from room, run 'participantDisconnected
-    room.once('disconnected', error => room.participants.forEach(participantDisconnected)); //If person randomally disconnects
-    thisroom = room; //Save room to global variable to user later
+  room.on("participantDisconnected", participantDisconnected); //When someone disconnects from room, run 'participantDisconnected
+  room.once("disconnected", error =>
+    room.participants.forEach(participantDisconnected)
+  ); //If person randomally disconnects
+  thisroom = room; //Save room to global variable to user later
 });
 
+function attachTracks(tracks, container) {
+  //Attach media to own screen
 
-function attachTracks(tracks, container) { //Attach media to own screen
-
-    if (!localVideoShow) {
-        tracks.forEach(function (track) {
-            container.append(track.attach());
-        });
-        localVideoShow = true;
-    }
-}
-
-function participantConnected(participant) { // When a new person connects to the room that you are in already, get their media.
-    console.log('Participant "%s" connected', participant.identity);
-
-    const div = $("#remote-media");
-    div.innerText = participant.identity;
-
-    participant.on('trackSubscribed', track => trackSubscribed(div, track));
-    participant.on('trackUnsubscribed', trackUnsubscribed);
-    participant.tracks.forEach(publication => {
-        if (publication.isSubscribed) {
-            trackSubscribed(div, publication.track);
-        }
+  if (!localVideoShow) {
+    tracks.forEach(function(track) {
+      container.append(track.attach());
     });
-
+    localVideoShow = true;
+  }
 }
 
+function participantConnected(participant) {
+  // When a new person connects to the room that you are in already, get their media.
+  console.log('Participant "%s" connected', participant.identity);
 
-function participantDisconnected(participant) { //When someone disconnects, close window
-    console.log('Participant "%s" disconnected', participant.identity);
+  const div = $("#remote-media");
+  div.innerText = participant.identity;
+
+  participant.on("trackSubscribed", track => trackSubscribed(div, track));
+  participant.on("trackUnsubscribed", trackUnsubscribed);
+  participant.tracks.forEach(publication => {
+    if (publication.isSubscribed) {
+      trackSubscribed(div, publication.track);
+    }
+  });
 }
 
-function trackSubscribed(div, track) { //Attach media to screen
-    div.append(track.attach());
+function participantDisconnected(participant) {
+  //When someone disconnects, close window
+  console.log('Participant "%s" disconnected', participant.identity);
 }
 
-function trackUnsubscribed(track) { //Remove media from screen
+function trackSubscribed(div, track) {
+  //Attach media to screen
+  div.append(track.attach());
+}
 
-    track.detach().forEach(element => element.remove());
+function trackUnsubscribed(track) {
+  //Remove media from screen
+
+  track.detach().forEach(element => element.remove());
 }
